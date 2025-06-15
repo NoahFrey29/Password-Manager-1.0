6831d8fa69fd1afa8616c591b4c82da9783f310b
Noah Frey <noahfrey29@gmail.com>
2025-06-15 11:14:40 -0400

    Completing set up in test file, learning how to write maps in elixir

--- test/password_generator_test.exs
+++ test/password_generator_test.exs
@@ -2,7 +2,27 @@ defmodule PasswordGeneratorTest do
   use ExUnit.Case
   doctest PasswordGenerator
 
-  test "greets the world" do
-    assert PasswordGenerator.hello() == :world
+  setup do
+    options = %{
+      "length" => "10",
+      "numbers" => "false",
+      "uppercase" => "false",
+      "symbols" => "false"
+    }
+
+    options_type = %{
+      lowercase: Enum.map(?a..?z, & <<?1>>),
+      numbers: Enum.map(0..9, & Integer.to_string(&1)),
+      uppercase: Enum.map(?A..?Z, & <<?1>>),
+      symbols: String.split("!@#$%^&*()_-+=[{}]|~;:?.><,", "", trim: true)
+    }
+
+    {:ok, result} = PasswordGenerator.generate(options)
+
+    %{
+      options_type: options_type,
+      result: result
+    }
+
   end
 end
